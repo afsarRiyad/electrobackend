0979c7dded0cb303e4220dc828735ebc8415b1c0
Nurul Afsar Riyad <nurulafsarriyad06@gmail.com>
2026-07-16 21:40:45 +0600

    create new products section

--- data/products.js
+++ data/products.js
@@ -747,4 +747,9 @@ export const homeV3Sections = [
     title: "Television Entertainment",
     productIds: [3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009, 3010, 3011, 3012, 3013, 3014, 3015],
   },
+  {
+    id: "laptops-and-computers",
+    title: "Laptops & Computers",
+    productIds: [2606, 2717, 2860, 2613, 2609, 2619],
+  },
 ];
